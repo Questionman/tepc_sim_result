0284a4cd79703544219776c096cf72d7c12b30b1
jihong <diti94@naver.com>
2018-11-29 10:36:31 +0900

    make nbin variable, so that easily changed

--- plotNtuple_JW.C
+++ plotNtuple_JW.C
@@ -1,6 +1,6 @@
 {
     gStyle->SetOptStat(0);
-    auto file = new TFile("B1_GCR_proton_initial.root");
+    auto file = new TFile("B1_GCR_proton_1uk.root");
     auto tree = (TTree *) file -> Get("B1");
 
     auto cvs = new TCanvas("cvs","",550,500);
@@ -8,13 +8,14 @@
     auto frame = new TH2D("frame","",500,1e-2,1e3,100,0.1,1e6);
     frame -> Draw();
   
-    Double_t* xboundary = new Double_t[500];
-    for (auto i=0;i<=500;i++)
+    auto nbin = 1000;
+    Double_t* xboundary = new Double_t[nbin];
+    for (auto i=0;i<=nbin;i++)
     {
-      xboundary[i]=pow(10,-2+5*i/500.);
+      xboundary[i]=pow(10,-2+5.*i/nbin);
     //printf("%f \n",xboundary[i]);
     }
-    auto hist = new TH1D("h1",";z (mm);Energy deposit (MeV)",500,xboundary);
+    auto hist = new TH1D("h1",";z (mm);Energy deposit (MeV)",nbin,xboundary);
     tree -> Draw("LET>>h1","","hist same L B *H");
     hist -> SetLineStyle(2);
     hist -> SetLineWidth(2);
@@ -23,17 +24,17 @@
     cvs -> SetLogy();
     cvs -> SetLogx();
 
-    auto hist2 = new TH1D("h2",";",500,xboundary);
+    auto hist2 = new TH1D("h2",";",nbin,xboundary);
     auto total_yifi = 0.;
-    for (auto ibin = 1; ibin <= 500; ++ibin)
+    for (auto ibin = 1; ibin <= nbin; ++ibin)
     {
         auto yi = hist -> GetXaxis() -> GetBinCenter(ibin);
         auto fi = hist -> GetBinContent(ibin);
         printf("ibin : %d yi : %f fi : %f\n",ibin, yi, fi);
-        hist2->SetBinContent(ibin, yi*fi*100);
+        hist2->SetBinContent(ibin, yi*fi*nbin/5);
         total_yifi += yi*fi;
     }
-    for (auto ibin = 1; ibin <=500; ++ibin)
+    for (auto ibin = 1; ibin <=nbin; ++ibin)
     {
       auto content = (hist2 -> GetBinContent(ibin)) / total_yifi;
       hist2 -> SetBinContent(ibin, content);             
